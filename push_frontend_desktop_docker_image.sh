b0cebb6c70862b563c08987fbfb8cec401931a98
Pratik Prajapati <pratik@42wolfsburg.de>
2024-10-17 09:52:52 +0200

    clean up

--- push_frontend_desktop_docker_image.sh
+++ push_frontend_desktop_docker_image.sh
@@ -6,19 +6,17 @@ set -e
 # Treat unset variables as an error
 set -u
 
-echo "Building tq-frontend-desktop image..."
+# Build frontend desktop docker image
 ./build_frontend_desktop_docker_image.sh
-echo "Image build completed."
 
 # Docker login
 docker login
-echo "Docker login successful."
 
 # fetch user name from docker login
 export DOCKER_USERNAME=$(docker info | grep Username | awk '{print $2}')
 
-echo "Tagging and pushing the image to Docker Hub..."
 # push above to docker hub
 docker tag tq-frontend-desktop:latest  $DOCKER_USERNAME/tq-frontend-desktop:latest
+
+# push to docker hub
 docker push $DOCKER_USERNAME/tq-frontend-desktop:latest
-echo "Image pushed to Docker Hub successfully."
--- push_frontend_web_light_docker_image.sh
+++ push_frontend_web_light_docker_image.sh
@@ -6,19 +6,17 @@ set -e
 # Treat unset variables as an error
 set -u
 
-echo "Building tq-frontend-web-light image..."
+# Build frontend web light docker image
 ./build_frontend_web_light_docker_image.sh
-echo "Image build completed."
 
 # Docker login
 docker login
-echo "Docker login successful."
 
 # fetch user name from docker login
 export DOCKER_USERNAME=$(docker info | grep Username | awk '{print $2}')
 
-echo "Tagging and pushing the image to Docker Hub..."
 # push above to docker hub
 docker tag tq-frontend-web-light:latest  $DOCKER_USERNAME/tq-frontend-web-light:latest
+
+# push to docker hub
 docker push $DOCKER_USERNAME/tq-frontend-web-light:latest
-echo "Image pushed to Docker Hub successfully."
